c9695c89938845bcc2d12060df676c9dbd34c3e4
Thinh11224 <thinhbui7779@gmail.com>
2025-05-23 17:30:58 +0700

    1. Hoàn thiện đăng nhập

--- Login.php
+++ Login.php
@@ -1,158 +1,163 @@
 <?php
-require 'components/header.php'
+require 'components/header.php';
+require 'db/connect.php';
+global $conn;
 ?>
-    <!-- Main container -->
-    <div
-      class="container d-flex justify-content-center align-items-center min-vh-100 border-5"
-    >
-      <!-- login-container -->
-      <div
-        class="row border border rounded-5 p-3 bg-white"
-        style="width: 930px; margin: auto"
-      >
-        <!-- left box -->
-        <div
-          class="col-md-6 rounded-4 justify-content-center align-content-center d-flex flex-column image-container"
-        >
-          <div>
-            <img
-              id="login-image"
-              src="./img/login-side-img.jpeg"
-              class="img-fluid rounded-5"
-              alt="Login Side"
-            />
-          </div>
-          <script>
-            function updateLoginImage() {
-              const img = document.getElementById("login-image");
-              const aspectRatio = window.innerHeight / window.innerWidth;
 
-              if (aspectRatio > 1.05) {
-                img.src = "./img/side-img-crop.png";
-              } else {
-                img.src = "./img/login-side-img.jpeg";
-              }
-            }
+<!-- Xử lý form đăng nhập -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
 
-            // Gọi ngay khi load
-            window.addEventListener("load", updateLoginImage);
-            // Gọi lại khi thay đổi kích thước cửa sổ
-            window.addEventListener("resize", updateLoginImage);
-          </script>
+  // Truy vấn database để kiểm tra thông tin đăng nhập và lấy role
+  $sql = "SELECT * FROM tbl_user WHERE username = '$username' AND password = '$password'";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $role = $row['role'];
+
+    // Kiểm tra role để chuyển hướng
+    if ($role == 0) {
+      // Admin
+      header("Location: admin.php");
+      exit();
+    } else if ($role == 1) {
+      // Người dùng
+      header("Location: index.php");
+      exit();
+    } else {
+      // Role không hợp lệ, chuyển về trang đăng nhập với lỗi
+      $error_message = "Role không hợp lệ.";
+    }
+  } else {
+    // Đăng nhập thất bại, hiển thị thông báo lỗi
+    $error_message = "Tên đăng nhập hoặc mật khẩu không chính xác.";
+  }
+}
+?>
+
+<body>
+  <!-- Main container -->
+  <div class="container d-flex justify-content-center align-items-center min-vh-100 border-5">
+    <!-- login-container -->
+    <div class="row border border rounded-5 p-3 bg-white" style="width: 930px; margin: auto">
+      <!-- left box -->
+      <div class="col-md-6 rounded-4 justify-content-center align-content-center d-flex flex-column image-container">
+        <div>
+          <img id="login-image" src="./img/login-side-img.jpeg" class="img-fluid rounded-5" alt="Login Side" />
         </div>
-        <!-- right box -->
-        <!-- tạo cột phải -->
-        <div class="col-md-6">
-          <!-- tạo 1 row ở cột phải -->
-          <div class="row align-items-center">
-            <div>
-              <p class="text-dark mt-4 mb-4" id="login-text">Sign in</p>
-            </div>
-            <!-- input username, password -->
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                class="form-control form-control-lg bg-light fs-6"
-                placeholder="Username"
-              />
-            </div>
-            <div class="input-group mb-4">
-              <input
-                type="password"
-                class="form-control form-control-lg bg-light fs-6"
-                placeholder="Password"
-              />
-            </div>
+        <script>
+          function updateLoginImage() {
+            const img = document.getElementById("login-image");
+            const aspectRatio = window.innerHeight / window.innerWidth;
 
-            <!-- tạo checkbox và forgot password trên cùng 1 hàng, dùng d-flex và justify-content-beetween
+            if (aspectRatio > 1.05) {
+              img.src = "./img/side-img-crop.png";
+            } else {
+              img.src = "./img/login-side-img.jpeg";
+            }
+          }
+
+          // Gọi ngay khi load
+          window.addEventListener("load", updateLoginImage);
+          // Gọi lại khi thay đổi kích thước cửa sổ
+          window.addEventListener("resize", updateLoginImage);
+        </script>
+      </div>
+      <!-- right box -->
+      <!-- tạo cột phải -->
+      <div class="col-md-6">
+        <!-- tạo 1 row ở cột phải -->
+        <div class="row align-items-center">
+          <div>
+            <p class="text-dark mt-4 mb-4" id="login-text">Sign in</p>
+          </div>
+          <!-- input username, password -->
+          <div class="input-group mb-3">
+            <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username" />
+          </div>
+          <div class="input-group mb-4">
+            <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" />
+          </div>
+
+          <!-- tạo checkbox và forgot password trên cùng 1 hàng, dùng d-flex và justify-content-beetween
               2 đối tượng này phải cùng cấp  -->
-            <div class="input-group d-flex justify-content-between">
-              <div>
-                <input type="checkbox" class="form-check-input" id="remember" />
-                <label for="remember"
-                  ><small class="text-dark">Remember me?</small></label
-                >
-              </div>
+          <div class="input-group d-flex justify-content-between">
+            <div>
+              <input type="checkbox" class="form-check-input" id="remember" />
+              <label for="remember"><small class="text-dark">Remember me?</small></label>
+            </div>
 
-              <div>
-                <small
-                  ><a href="#" class="text-dark float-end"
-                    >Forgot password?</a
-                  ></small
-                >
+            <div>
+              <small><a href="#" class="text-dark float-end">Forgot password?</a></small>
+            </div>
+          </div>
+          <!-- Button login -->
+          <div class="input-group mb-lg-3 mb-md-2 mb-sm-2">
+            <button type="button" class="btn btn-dark btn-lg mt-4 rounded-5 w-100" id="login-btn">
+              Sign in
+            </button>
+          </div>
+          <!-- line -->
+          <div>
+            <hr style="border: 1px solid #9da1a6" />
+          </div>
+          <div class="mb-3">
+            <small class="text-dark">Or login with</small>
+          </div>
+          <!-- tạo 1 row với 4 cols để thêm 4 button trong cùng 1 hàng -->
+          <div class="row text-center" style="margin-left: 2px">
+            <!-- tạo col thứ nhất -->
+            <div class="col-3">
+              <div class="input-group mb-lg-2">
+                <button class="btn bg-light w-100 fs-6">
+                  <img src="./img/google.png" style="width: 30px" />
+                </button>
               </div>
             </div>
-            <!-- Button login -->
-            <div class="input-group mb-lg-3 mb-md-2 mb-sm-2">
-              <button
-                type="button"
-                class="btn btn-dark btn-lg mt-4 rounded-5 w-100"
-                id="login-btn"
-              >
-                Sign in
-              </button>
-            </div>
-            <!-- line -->
-            <div><hr style="border: 1px solid #9da1a6" /></div>
-            <div class="mb-3">
-              <small class="text-dark">Or login with</small>
-            </div>
-            <!-- tạo 1 row với 4 cols để thêm 4 button trong cùng 1 hàng -->
-            <div class="row text-center" style="margin-left: 2px">
-              <!-- tạo col thứ nhất -->
-              <div class="col-3">
-                <div class="input-group mb-lg-2">
-                  <button class="btn bg-light w-100 fs-6">
-                    <img src="./img/google.png" style="width: 30px" />
-                  </button>
-                </div>
-              </div>
-              <!-- tạo col thứ 2 -->
-              <div class="col-3">
-                <div class="input-group mb-lg-2">
-                  <button class="btn bg-light w-100 fs-6">
-                    <img src="./img/facebook2.png" style="width: 30px" />
-                  </button>
-                </div>
-              </div>
-              <!-- tạo col thứ 3 -->
-              <div class="col-3">
-                <div class="input-group mb-lg-2">
-                  <button class="btn bg-light w-100 fs-6">
-                    <img src="./img/instagram.png" style="width: 30px" />
-                  </button>
-                </div>
-              </div>
-              <!-- tạo col thứ 4 -->
-              <div class="col-3">
-                <div class="input-group mb-lg-2">
-                  <button class="btn bg-light w-100 fs-6">
-                    <img src="./img/twitter.png" style="width: 30px" />
-                  </button>
-                </div>
+            <!-- tạo col thứ 2 -->
+            <div class="col-3">
+              <div class="input-group mb-lg-2">
+                <button class="btn bg-light w-100 fs-6">
+                  <img src="./img/facebook2.png" style="width: 30px" />
+                </button>
               </div>
             </div>
-            <!-- tạo need an account và sign up cùng trên 1 hàng, lại dùng d-flex justify-content -->
-            <div class="justify-content d-flex mt-4">
-              <div>
-                <small class="text" style="margin-right: 5px"
-                  >Need an account?</small
-                >
+            <!-- tạo col thứ 3 -->
+            <div class="col-3">
+              <div class="input-group mb-lg-2">
+                <button class="btn bg-light w-100 fs-6">
+                  <img src="./img/instagram.png" style="width: 30px" />
+                </button>
               </div>
+            </div>
+            <!-- tạo col thứ 4 -->
+            <div class="col-3">
+              <div class="input-group mb-lg-2">
+                <button class="btn bg-light w-100 fs-6">
+                  <img src="./img/twitter.png" style="width: 30px" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- tạo need an account và sign up cùng trên 1 hàng, lại dùng d-flex justify-content -->
+          <div class="justify-content d-flex mt-4">
+            <div>
+              <small class="text" style="margin-right: 5px">Need an account?</small>
+            </div>
 
-              <div>
-                <small
-                  ><a href="./Registration.php" class="text-success"
-                    >Sign up</a
-                  ></small
-                >
-              </div>
+            <div>
+              <small><a href="./Registration.php" class="text-success">Sign up</a></small>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <!-- bootstrap js -->
-    <script src="./asset/bootstrap/js/bootstrap.bundle.min.js"></script>
-  </body>
+  </div>
+  <!-- bootstrap js -->
+  <script src="./asset/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
 </html>
