457fd81162dab54dd9d14050fe18f906de621b35
Thinh11224 <thinhbui7779@gmail.com>
2025-05-23 17:55:28 +0700

    2. Hoàn thiện đăng nhập

--- Login.php
+++ Login.php
@@ -68,91 +68,98 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       </div>
       <!-- right box -->
       <!-- tạo cột phải -->
+
       <div class="col-md-6">
-        <!-- tạo 1 row ở cột phải -->
-        <div class="row align-items-center">
-          <div>
-            <p class="text-dark mt-4 mb-4" id="login-text">Sign in</p>
-          </div>
-          <!-- input username, password -->
-          <div class="input-group mb-3">
-            <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username" />
-          </div>
-          <div class="input-group mb-4">
-            <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" />
-          </div>
+        <!-- Form đăng nhập -->
+        <form action="login.php" method="post">
+          <!-- tạo 1 row ở cột phải -->
+          <div class="row align-items-center">
+            <div>
+              <p class="text-dark mt-4 mb-4" id="login-text">Sign in</p>
+            </div>
+            <!-- input username, password -->
+            <div class="input-group mb-3">
+              <input type="text" id="username" name="username" class="form-control form-control-lg bg-light fs-6"
+                placeholder="Username" required />
+            </div>
 
-          <!-- tạo checkbox và forgot password trên cùng 1 hàng, dùng d-flex và justify-content-beetween
+            <div class="input-group mb-4">
+              <input type="password" id="password" name="password" class="form-control form-control-lg bg-light fs-6"
+                placeholder="Password" required />
+            </div>
+
+            <!-- tạo checkbox và forgot password trên cùng 1 hàng, dùng d-flex và justify-content-beetween
               2 đối tượng này phải cùng cấp  -->
-          <div class="input-group d-flex justify-content-between">
-            <div>
-              <input type="checkbox" class="form-check-input" id="remember" />
-              <label for="remember"><small class="text-dark">Remember me?</small></label>
-            </div>
+            <div class="input-group d-flex justify-content-between">
+              <div>
+                <input type="checkbox" class="form-check-input" id="remember" />
+                <label for="remember"><small class="text-dark">Remember me?</small></label>
+              </div>
 
+              <div>
+                <small><a href="#" class="text-dark float-end">Forgot password?</a></small>
+              </div>
+            </div>
+            <!-- Button login -->
+            <div class="input-group mb-lg-3 mb-md-2 mb-sm-2">
+              <button type="submit" class="btn btn-dark btn-lg mt-4 rounded-5 w-100" id="login-btn">
+                Sign in
+              </button>
+            </div>
+            <!-- line -->
             <div>
-              <small><a href="#" class="text-dark float-end">Forgot password?</a></small>
+              <hr style="border: 1px solid #9da1a6" />
             </div>
-          </div>
-          <!-- Button login -->
-          <div class="input-group mb-lg-3 mb-md-2 mb-sm-2">
-            <button type="button" class="btn btn-dark btn-lg mt-4 rounded-5 w-100" id="login-btn">
-              Sign in
-            </button>
-          </div>
-          <!-- line -->
-          <div>
-            <hr style="border: 1px solid #9da1a6" />
-          </div>
-          <div class="mb-3">
-            <small class="text-dark">Or login with</small>
-          </div>
-          <!-- tạo 1 row với 4 cols để thêm 4 button trong cùng 1 hàng -->
-          <div class="row text-center" style="margin-left: 2px">
-            <!-- tạo col thứ nhất -->
-            <div class="col-3">
-              <div class="input-group mb-lg-2">
-                <button class="btn bg-light w-100 fs-6">
-                  <img src="./img/google.png" style="width: 30px" />
-                </button>
+            <div class="mb-3">
+              <small class="text-dark">Or login with</small>
+            </div>
+            <!-- tạo 1 row với 4 cols để thêm 4 button trong cùng 1 hàng -->
+            <div class="row text-center" style="margin-left: 2px">
+              <!-- tạo col thứ nhất -->
+              <div class="col-3">
+                <div class="input-group mb-lg-2">
+                  <button class="btn bg-light w-100 fs-6">
+                    <img src="./img/google.png" style="width: 30px" />
+                  </button>
+                </div>
+              </div>
+              <!-- tạo col thứ 2 -->
+              <div class="col-3">
+                <div class="input-group mb-lg-2">
+                  <button class="btn bg-light w-100 fs-6">
+                    <img src="./img/facebook2.png" style="width: 30px" />
+                  </button>
+                </div>
+              </div>
+              <!-- tạo col thứ 3 -->
+              <div class="col-3">
+                <div class="input-group mb-lg-2">
+                  <button class="btn bg-light w-100 fs-6">
+                    <img src="./img/instagram.png" style="width: 30px" />
+                  </button>
+                </div>
+              </div>
+              <!-- tạo col thứ 4 -->
+              <div class="col-3">
+                <div class="input-group mb-lg-2">
+                  <button class="btn bg-light w-100 fs-6">
+                    <img src="./img/twitter.png" style="width: 30px" />
+                  </button>
+                </div>
               </div>
             </div>
-            <!-- tạo col thứ 2 -->
-            <div class="col-3">
-              <div class="input-group mb-lg-2">
-                <button class="btn bg-light w-100 fs-6">
-                  <img src="./img/facebook2.png" style="width: 30px" />
-                </button>
+            <!-- tạo need an account và sign up cùng trên 1 hàng, lại dùng d-flex justify-content -->
+            <div class="justify-content d-flex mt-4">
+              <div>
+                <small class="text" style="margin-right: 5px">Need an account?</small>
               </div>
-            </div>
-            <!-- tạo col thứ 3 -->
-            <div class="col-3">
-              <div class="input-group mb-lg-2">
-                <button class="btn bg-light w-100 fs-6">
-                  <img src="./img/instagram.png" style="width: 30px" />
-                </button>
-              </div>
-            </div>
-            <!-- tạo col thứ 4 -->
-            <div class="col-3">
-              <div class="input-group mb-lg-2">
-                <button class="btn bg-light w-100 fs-6">
-                  <img src="./img/twitter.png" style="width: 30px" />
-                </button>
-              </div>
-            </div>
-          </div>
-          <!-- tạo need an account và sign up cùng trên 1 hàng, lại dùng d-flex justify-content -->
-          <div class="justify-content d-flex mt-4">
-            <div>
-              <small class="text" style="margin-right: 5px">Need an account?</small>
-            </div>
 
-            <div>
-              <small><a href="./Registration.php" class="text-success">Sign up</a></small>
+              <div>
+                <small><a href="./Registration.php" class="text-success">Sign up</a></small>
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   </div>
