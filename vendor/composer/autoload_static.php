346be6c9d0c2d31249387caac2e90a1667d8ea41
VuIT <chuminhvuofficial@gmail.com>
2025-06-27 00:07:46 +0700

    thêm composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -24,6 +24,8 @@ class ComposerStaticInit566b298913829ffc1df98fbece4f9631
             'Psr\\Log\\' => 8,
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Cache\\' => 10,
+            'PayOS\\' => 6,
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
         'M' => 
         array (
@@ -59,6 +61,14 @@ class ComposerStaticInit566b298913829ffc1df98fbece4f9631
         'Psr\\Cache\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/cache/src',
+        ),
+        'PayOS\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/payos/payos/src',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Monolog\\' => 
         array (
